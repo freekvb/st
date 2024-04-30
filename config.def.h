021d899e51d6aa629a9ede756b98d44ef276f6e0
freekvb <freekvb@gmail.com>
2024-04-30 22:11:14 +0200

    colors by pywal

--- config.def.h
+++ config.def.h
@@ -94,46 +94,8 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 4;
 
-/* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
-};
-
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+/* Terminal colors with pywall */
+#include "/home/fvb/.cache/wal/colors-wal-st.h"
 
 /*
  * Default shape of cursor
